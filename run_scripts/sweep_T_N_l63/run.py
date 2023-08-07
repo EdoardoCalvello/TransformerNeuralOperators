38d6ac07bf9135c35a8f5eb88f9cbc79c3fe2e13
mattlevine22 <mattlevine22@gmail.com>
2023-08-07 11:08:39 -0700

    updating run scripts with simpler naming

--- run_scripts/sweep_T_N_l63/run.py
+++ run_scripts/sweep_T_N_l63/run.py
@@ -6,7 +6,8 @@ import argparse
 
 # use argparse to get command line argument for which experiment to run
 parser = argparse.ArgumentParser()
-parser.add_argument('--project_name', type=str, default='debug')
+parser.add_argument('--project_name', type=str,
+                    default='sweep_sequence_length-fixed_data_quantity')
 parser.add_argument('--id', type=int, default=0)
 args = parser.parse_args()
 
@@ -22,7 +23,7 @@ exp_dict = {
     'n_trajectories_test': [100],
     'seq_len': [5, 10, 50, 100, 200, 500, 1000],
     'sample_rate': [0.01],
-    'batch_size': [128],
+    'batch_size': [64],
     'dyn_sys_name': ['Lorenz63'],
     'input_inds': [[0]],
     'output_inds': [[1,2]],
--- run_scripts/sweep_T_N_l63/run.sh
+++ run_scripts/sweep_T_N_l63/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=big_run_lorenz        # Name of the job
-#SBATCH --array=0-2                   # Number of tasks in the array (e.g., 10 jobs)
+#SBATCH --job-name=sweep_T_N        # Name of the job
+#SBATCH --array=0-6                   # Number of tasks in the array (e.g., 10 jobs)
 #SBATCH --gres=gpu:1                   # Number of GPUs to allocate per job (1 GPU per job)
 #SBATCH --time=02:00:00                # Maximum runtime for each job (hh:mm:ss)
 #SBATCH --ntasks=1         # number of processor cores (i.e. tasks)
@@ -21,4 +21,4 @@ module load cuda/11.8
 # mkdir -p slurm_logs
 
 # Run your Python script with the GPU device specified (assuming your script is named 'your_script.py')
-python run.py --id $SLURM_ARRAY_TASK_ID --project_name debug
+python run.py --id $SLURM_ARRAY_TASK_ID
