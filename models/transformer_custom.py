ef9d21a624430528a87a5af30dad1c28f55fc7be
EdoardoCalvello <55067458+EdoardoCalvello@users.noreply.github.com>
2023-08-07 00:29:31 -0700

    Create EncoderDecoder

--- models/transformer_custom.py
+++ models/transformer_custom.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
-import pdb
 
 class MultiHeadAttention(pl.LightningModule):
     def __init__(self, d_model, num_heads):
@@ -46,3 +45,31 @@ class MultiHeadAttention(pl.LightningModule):
         attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
         output = self.W_o(self.combine_heads(attn_output))
         return output
+
+
+class PositionalEncoding(pl.LightningModule):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+        
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        self.register_buffer('pe', pe.unsqueeze(0))
+        
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+    
+    
+class PositionWiseFeedForward(pl.LightningModule):
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
