aea8c0e633069ff14e2ca91c6aaaa93b5994cf1a
mattlevine22 <mattlevine22@gmail.com>
2023-08-09 21:58:54 -0700

    more data in dt run with continuous-time positional encoding

--- run_scripts/basic_l63_trainDt/run.py
+++ run_scripts/basic_l63_trainDt/run.py
@@ -14,9 +14,9 @@ args = parser.parse_args()
 exp_dict = {
     'project_name': [args.project_name],
     # data settings
-    'n_trajectories_train': [1000], # smaller dataset for debugging
-    'n_trajectories_val': [100],
-    'n_trajectories_test': [100],
+    'n_trajectories_train': [5000], # smaller dataset for debugging
+    'n_trajectories_val': [500],
+    'n_trajectories_test': [500],
     'T': [2],
     'train_sample_rate': [0.5e-2, 1e-2, 2e-2],
     'test_sample_rates': [[0.5e-2, 1e-2, 2e-2]],
