c17dd40fb2cdfa59839d9861df97b5edc2eba182
mattlevine22 <mattlevine22@gmail.com>
2023-09-16 11:44:38 -0400

    modify darcy flow run to do more epochs and larger batch for HPC

--- run_scripts/basic_darcy_debug/run.py
+++ run_scripts/basic_darcy_debug/run.py
@@ -6,7 +6,7 @@ import argparse
 
 # use argparse to get command line argument for which experiment to run
 parser = argparse.ArgumentParser()
-parser.add_argument('--project_name', type=str, default='darcy_debug')
+parser.add_argument('--project_name', type=str, default='darcy_flow_1.0')
 parser.add_argument('--id', type=int, default=0)
 args = parser.parse_args()
 
@@ -21,15 +21,15 @@ exp_dict = {
     'domain_dim': [2], # 1 for timeseries, 2 for 2D spatial
     'train_sample_rate': [2],
     'test_sample_rates': [[1,2,4]],
-    'batch_size': [4],
-    'dyn_sys_name': ['darcy_low_res'],
+    'batch_size': [4, 16, 64],
+    'dyn_sys_name': ['darcy_low_res', 'darcy_high_res'],
     # optimizer settings
     'learning_rate': [1e-3],
     'dropout': [1e-4],
     'lr_scheduler_params': [
                             {'patience': 2, 'factor': 0.5},
                              ],
-    'max_epochs': [5],
+    'max_epochs': [100],
     'monitor_metric': ['loss/val/mse'],
     # model settings (modest model size for debugging)
     'include_y0_input': [False], #['uniform', 'staggered', False],
