4c6b788b4ccffb09fa8f78ddb4d97b570f1343a6
EdoardoCalvello <55067458+EdoardoCalvello@users.noreply.github.com>
2024-04-24 14:44:55 -0700

    update

---
+++ paper_run_scripts/2d_experiments_vanilla/run.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('../../')
+from models.SimpleEncoder.runner import Runner
+from utils import dict_combiner
+import argparse
+
+# use argparse to get command line argument for which experiment to run
+parser = argparse.ArgumentParser()
+parser.add_argument('--project_name', type=str, default='darcy_flow_1.0')
+parser.add_argument('--id', type=int, default=0)
+args = parser.parse_args()
+
+# once working, need to update readme with:
+# conda install -c anaconda scikit-learn
+
+# build a dict of experimental conditions
+exp_dict = {
+    'project_name': [args.project_name],
+    # data settings
+    'split_frac': [{'train': 0.9, 'val': 0.05, 'test': 0.05}],
+    'random_state': [0],
+    'domain_dim': [2], # 1 for timeseries, 2 for 2D spatial
+    'train_sample_rate': [25],
+    'test_sample_rates': [[12,25,50]],
+    'pos_enc_coeff': [1],#[0, 1, 2, 3],
+    'batch_size': [1],
+    'dyn_sys_name': ['darcy_high_res_discontinuous'],
+    # optimizer settings
+    'learning_rate': [1e-4],
+    'dropout': [1e-4],
+    'lr_scheduler_params': [
+                            {'patience': 2, 'factor': 0.5},
+                             ],
+    'max_epochs': [100],
+    'monitor_metric': ['loss/val/mse'],
+    # model settings (modest model size for debugging)
+    'include_y0_input': [False], #['uniform', 'staggered', False],
+    'd_model': [128],
+    'nhead': [8],
+    'num_layers': [6],
+    'dim_feedforward': [128],
+    'activation': ['gelu'],
+    'use_positional_encoding': [False], #['continuous', 'discrete', False],
+    'append_position_to_x': [True], #[True, False],
+}
+
+exp_list = dict_combiner(exp_dict)
+
+# Print the length of the experiment list
+print('Number of experiments to sweep: ', len(exp_list))
+
+# run the experiment
+Runner(**exp_list[args.id])
+
+
