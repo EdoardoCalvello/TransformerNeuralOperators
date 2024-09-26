449fb8001ce25529a74d675c6847f2beb1418b19
EdoardoCalvello <55067458+EdoardoCalvello@users.noreply.github.com>
2024-09-26 14:06:33 -0700

    clean version

--- models/transformer_custom.py
+++ models/transformer_custom.py
@@ -8,14 +8,13 @@ import copy
 class ScaledDotProductAttention(nn.Module):
     def __init__(self, d_k, dropout=0.1):
         super(ScaledDotProductAttention, self).__init__()
-        #d_model* or just d_model?
+
         self.scale = nn.Parameter(torch.sqrt(torch.FloatTensor([d_k])), requires_grad=False)
         self.dropout = nn.Dropout(dropout)
 
     def custom_softmax(self, x, coords=None, dim=-1):
 
-        #exp_x = torch.exp(x - x.max(dim=dim, keepdim=True)[0])
-        exp_x = torch.exp(x)
+        exp_x = torch.exp(x - x.max(dim=dim, keepdim=True)[0])
         #reweighting of exp_x along seq_len dimension
         if coords is not None:
             softmax_x = exp_x / (0.5*coords*(exp_x[...,1:]+exp_x[...,:-1])).sum(dim=dim, keepdim=True)
@@ -147,8 +146,11 @@ class SpectralConv2d(nn.Module):
         self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
         self.modes2 = modes2
         self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
-        self.weights2 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        #use torch.view_as_real if using ddp
+        #self.weights1 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        #self.weights2 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        self.weights1 = nn.Parameter(self.scale * (torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        self.weights2 = nn.Parameter(self.scale * (torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
     # Complex multiplication
     def compl_mul2d(self, input, weights):
         # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
@@ -159,10 +161,15 @@ class SpectralConv2d(nn.Module):
         x_ft = torch.fft.rfft2(x)
         # Multiply relevant Fourier modes
         out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        #use torch.view_as_complex if using ddp
+        #out_ft[:, :, :self.modes1, :self.modes2] = \
+        #    self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], torch.view_as_complex(self.weights1))
+        #out_ft[:, :, -self.modes1:, :self.modes2] = \
+        #    self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], torch.view_as_complex(self.weights2))
         out_ft[:, :, :self.modes1, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], torch.view_as_complex(self.weights1))
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
         out_ft[:, :, -self.modes1:, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], torch.view_as_complex(self.weights2))
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
         #Return to physical space
         x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
         return x
@@ -318,8 +325,11 @@ class SpectralConv2d_in(nn.Module):
         self.modes2 = modes2
 
         self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
-        self.weights2 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        #use torch.view_as_real if using ddp
+        #self.weights1 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        #self.weights2 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        self.weights1 = nn.Parameter(self.scale * (torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
+        self.weights2 = nn.Parameter(self.scale * (torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat)))
 
     # Complex multiplication
     def compl_mul2d(self, input, weights):
@@ -334,10 +344,15 @@ class SpectralConv2d_in(nn.Module):
 
         # Multiply relevant Fourier modes
         out_ft = torch.zeros(batchsize, num_patches, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        #use torch.view_as_complex if using ddp
+        #out_ft[:,:, :, :self.modes1, :self.modes2] = \
+            #self.compl_mul2d(x_ft[:, :,:, :self.modes1, :self.modes2], torch.view_as_complex(self.weights1))
+        #out_ft[:,:, :, -self.modes1:, :self.modes2] = \
+            #self.compl_mul2d(x_ft[:, :,:, -self.modes1:, :self.modes2], torch.view_as_complex(self.weights2))
         out_ft[:,:, :, :self.modes1, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :,:, :self.modes1, :self.modes2], torch.view_as_complex(self.weights1))
+            self.compl_mul2d(x_ft[:, :,:, :self.modes1, :self.modes2], self.weights1)
         out_ft[:,:, :, -self.modes1:, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :,:, -self.modes1:, :self.modes2], torch.view_as_complex(self.weights2))
+            self.compl_mul2d(x_ft[:, :,:, -self.modes1:, :self.modes2], self.weights2)
 
         #Return to physical space
         x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
@@ -480,8 +495,11 @@ class SpectralConv2d_Attention(nn.Module):
         self.modes2 = modes2
         self.nhead = nhead
         self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.nhead, dtype=torch.cfloat)))
-        self.weights2 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.nhead, dtype=torch.cfloat)))
+        #use torch.view_as_real if using ddp
+        #self.weights1 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.nhead, dtype=torch.cfloat)))
+        #self.weights2 = nn.Parameter(self.scale * torch.view_as_real(torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.nhead, dtype=torch.cfloat)))
+        self.weights1 = nn.Parameter(self.scale * (torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.nhead, dtype=torch.cfloat)))
+        self.weights2 = nn.Parameter(self.scale * (torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.nhead, dtype=torch.cfloat)))
 
     # Complex multiplication
     def compl_mul2d(self, input, weights):
@@ -498,10 +516,15 @@ class SpectralConv2d_Attention(nn.Module):
         x_ft = torch.fft.rfft2(x)
         # Multiply relevant Fourier modes
         out_ft = torch.zeros(batchsize, num_patches, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, self.nhead, dtype=torch.cfloat, device=x.device)
+        #use torch.view_as_complex if using ddp
+        #out_ft[:, :, :, :self.modes1, :self.modes2, :] = \
+        #    self.compl_mul2d(x_ft[:, :, :, :self.modes1, :self.modes2], torch.view_as_complex(self.weights1))
+        #out_ft[:, :, :, -self.modes1:, :self.modes2, :] = \
+        #    self.compl_mul2d(x_ft[:, :, :, -self.modes1:, :self.modes2], torch.view_as_complex(self.weights2))
         out_ft[:, :, :, :self.modes1, :self.modes2, :] = \
-            self.compl_mul2d(x_ft[:, :, :, :self.modes1, :self.modes2], torch.view_as_complex(self.weights1))
+            self.compl_mul2d(x_ft[:, :, :, :self.modes1, :self.modes2], self.weights1)
         out_ft[:, :, :, -self.modes1:, :self.modes2, :] = \
-            self.compl_mul2d(x_ft[:, :, :, -self.modes1:, :self.modes2], torch.view_as_complex(self.weights2))
+            self.compl_mul2d(x_ft[:, :, :, -self.modes1:, :self.modes2], self.weights2)
         #Return to physical space
         x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)), dim=(-3, -2))
         #####
@@ -617,106 +640,5 @@ class TransformerEncoderLayer_Conv(nn.Module):#
         x = x + self.dropout(ff_output)
         if self.do_layer_norm:
             x = self.norm2(x)
-
-        return x
-
-
-###############################################################################################################
-###############################################################################################################
-#MODULES FOR INTEGRAL QKV TRANSFORMER ARCHITECTURE WITH GLOBAL CONV
-###############################################################################################################
-###############################################################################################################
-
-
-class TransformerEncoderLayer_Conv_E(nn.Module):#
-
-    def __init__(self, d_model, nhead, dropout=0.1, activation="relu", norm_first=True, do_layer_norm=True, dim_feedforward=2048, modes=None, patch_size=1, im_size=64, batch_first=True):
-        super(TransformerEncoderLayer_Conv_E, self).__init__()
-        # Self-attention layer
-        if modes is None:
-            modes2 = patch_size//2+1
-            modes1 = patch_size//2+1
-        else:
-            modes1 = modes[0]
-            modes2 = modes[1]
-
-        self.patch_size = patch_size
-        self.im_size = im_size
-        self.size_row = im_size
-        self.size_col = im_size
-        self.d_model = d_model
-
-        #or im_size?
-        self.self_attn = MultiheadAttention_Conv(d_model, nhead, modes1, modes2, im_size, dropout=dropout)
-        # Feedforward layer
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
-        # Layer normalization
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.norm3 = nn.LayerNorm(d_model)
-        # Activation function
-
-        self.fourier_conv = SpectralConv2d(d_model, d_model, modes1+4, modes2+4)
-
-        self.activation = getattr(F, activation)
-        # Dropout
-        self.dropout = nn.Dropout(dropout)
-        # Whether to normalize before the self-attention layer
-        self.norm_first = norm_first
-        # Whether to apply layer normalization at the end
-        self.do_layer_norm = do_layer_norm
-
-    def chop_to_patches(self,x):
-
-        num_channels = x.shape[1]
-        # - shape is now (batch_size, num_channels, num_patch_per_row, patch_size, num_patch_per_col, patch_size)
-        chopped_x = x.reshape(x.shape[0], num_channels, self.size_row // self.patch_size, self.patch_size,
-                      self.size_col // self.patch_size, self.patch_size)
-
-        # - shape is now (batch_size, num_patch_per_row, num_patch_per_col, num_channels, patch_size, patch_size)
-        chopped_x = torch.permute(chopped_x, (0,2,4,1,3,5))
-
-
-        chopped_x = torch.permute(chopped_x, (0,3,4,5,1,2))
-        # - shape is now (batch_size, num_channels, patch_size, patch_size, num_patch_per_row, num_patch_per_col)
-        chopped_x = chopped_x.reshape(chopped_x.shape[0], chopped_x.shape[1], chopped_x.shape[2], chopped_x.shape[3], chopped_x.shape[4]*chopped_x.shape[5])
-        # - shape is now (batch_size, num_channels, patch_size, patch_size, num_patches)
-        chopped_x = torch.permute(chopped_x, (0,4,2,3,1))
-        # - shape is now (batch_size, num_patches, patch_size, patch_size, num_channels)
-
-
-        return chopped_x
-
-    def stitch_patches(self,x):
-
-        x = x.reshape(x.shape[0],self.im_size//self.patch_size, self.im_size//self.patch_size ,self.patch_size,self.patch_size, self.d_model)
-        x = x.permute(0,5,1,3,2,4)
-        x = x.reshape(x.shape[0],self.d_model,self.size_row,self.size_col)
-
-        return x
-
-    def forward(self, x, mask=None):
-        if self.norm_first:
-            x = self.norm1(x)
-        # Self-attention
-        attn_output = self.self_attn(x, key_padding_mask=mask)
-        # Residual connection and layer normalization
-        x = x + self.dropout(attn_output)
-        if not self.norm_first:
-            x = self.norm1(x)
-        # Feedforward layer
-        ff_output = self.linear2(self.dropout(self.activation(self.linear1(x))))
-        # Residual connection and layer normalization
-        x = x + self.dropout(ff_output)
-        if self.do_layer_norm:
-            x = self.norm2(x)
-
-        x_residual = self.stitch_patches(x)
-        x_conv = self.fourier_conv(x_residual)
-        x_conv = x_residual + x_conv
-        x = self.chop_to_patches(x_conv)
-        x = self.norm3(x)
 
         return x
